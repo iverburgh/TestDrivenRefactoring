f4cf17c1058662afca006cfb33eda88b704228ec
Ivo Verburgh <I.Verburgh@4dotnet.nl>
2023-12-07 17:18:11 +0100

    Initial commit

---
+++ TestDrivenRefactoring.Tests/VideoStoreTests.cs
@@ -0,0 +1,44 @@
+using System.Collections.Immutable;
+using FluentAssertions;
+
+namespace TestDrivenRefactoring.Tests
+{
+    public class VideoStoreTests
+    {
+        private readonly VideoStore _subject; 
+
+        public VideoStoreTests()
+        {
+            _subject = new VideoStore();
+        }
+
+        [Fact]
+        public void Statement_WhenValidParametersAreGiven_ThenCorrectOutput()
+        {
+            // Arrange
+            var plays = new Dictionary<string, Play>
+            {
+                {"hamlet", new Play("Hamlet", PayType.Tragedy)},
+                {"as-like", new Play("As You Like It", PayType.Comedy)},
+                {"othello", new Play("Othello", PayType.Tragedy)}
+            }.ToImmutableDictionary();
+
+            var invoice = new Invoice("BigCo", new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40)
+            });
+
+            var expectedResult = "Statement for BigCo\r\n" +
+                                 "  Hamlet: € 650,00 (55 seats)\r\n  As You Like It: € 580,00 (35 seats)\r\n  Othello: € 500,00 (40 seats)\r\n" +
+                                 "Amount owed is € 1.730,00\r\nYou earned 47 credits";
+
+            // Act
+            var result = _subject.Statement(invoice, plays);
+
+            // Assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
---
+++ TestDrivenRefactoring/VideoStore.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace TestDrivenRefactoring
+{
+    public class VideoStore
+    {
+        public string Statement(Invoice invoice, IReadOnlyDictionary<string, Play> plays)
+        {
+            int totalAmount = 0;
+            int volumeCredits = 0;
+
+            IFormatProvider format = new CultureInfo("en-US");
+
+            var result = new StringBuilder().AppendLine($"Statement for {invoice.Customer}");
+
+            foreach (var perf in invoice.Performances)
+            {
+                var play = plays[perf.PlayId];
+                int thisAmount;
+
+                switch (play.PayType)
+                {
+                    case PayType.Tragedy:
+                        thisAmount = 40000;
+                        if (perf.Audience > 30) thisAmount += 1000 * (perf.Audience - 30);
+
+                        break;
+
+                    case PayType.Comedy:
+                        thisAmount = 30000;
+                        if (perf.Audience > 20) thisAmount += 10000 + 500 * (perf.Audience - 20);
+                        thisAmount += 300 * perf.Audience;
+                        break;
+
+                    default:
+                        throw new Exception($"unknown type: {play.PayType}");
+                }
+
+                // add volume credits
+                volumeCredits += Math.Max(perf.Audience - 30, 0);
+                // add extra credit for every ten comedy attendees
+                if (PayType.Comedy == play.PayType) volumeCredits += (int)Math.Floor((decimal)perf.Audience / 5);
+
+                // print line for this order
+                result.AppendFormat(format, $"  {play.Name}: {thisAmount / 100:C}");
+                result.AppendLine($" ({perf.Audience} seats)");
+                totalAmount += thisAmount;
+            }
+
+            result.AppendFormat(format, $"Amount owed is {totalAmount / 100:C}");
+            result.AppendLine("");
+            result.Append($"You earned {volumeCredits} credits");
+            return result.ToString();
+        }
+    }
+
+    public enum PayType : byte
+    {
+        Tragedy,
+        Comedy
+    }
+
+
+    public class Play
+    {
+        public string Name { get; }
+        public PayType PayType { get; }
+
+        public Play(string name, PayType payType)
+        {
+            Name = name;
+            PayType = payType;
+        }
+    }
+
+    public class Performance
+    {
+        public int Audience { get; }
+        public string PlayId { get; }
+
+        public Performance(string playId, int audience)
+        {
+            PlayId = playId;
+            Audience = audience;
+        }
+    }
+
+    public class Invoice
+    {
+        public string Customer { get; }
+        public ImmutableList<Performance> Performances { get; }
+
+        [JsonConstructor] // JsonSerializer.Deserialize will use this ctor 
+        public Invoice(string customer, ImmutableList<Performance> performances)
+        {
+            Customer = customer;
+            Performances = performances;
+        }
+
+        public Invoice(string customer, IEnumerable<Performance> performances)
+            : this(customer, performances.ToImmutableList())
+        {
+        }
+    }
+}
